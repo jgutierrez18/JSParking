15edf59a0c8579ce79fad0dc5d1f97072e60ee4d
jgutierrez <jepetoto18@gmail.com>
2017-05-20 20:38:33 -0500

    20/05/2017

--- app/src/main/java/com/jgutierrez/jsparking/InicioActivity.java
+++ app/src/main/java/com/jgutierrez/jsparking/InicioActivity.java
@@ -1,7 +1,9 @@
 package com.jgutierrez.jsparking;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -12,5 +14,24 @@ public class InicioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
     }
+    Button btnlogin = (Button) findViewById(R.id.btnlogin);
 
+       // btnlogin.setOnClickListener(new View.OnClickListener() {
+        //@Override
+        //public void onClick(View v) {
+          //Intent intent = new Intent(v.getContext(),LoginActivity.class);
+            //startActivityForResult(intent,0);
+
+        //}
+    //};
+
+    Button btnregistro = (Button) findViewById(R.id.btnregistro);
+        //btnregistro.setOnClickListener(new View.OnClickListener() {
+        //@Override
+        //public void onClick(View v) {
+          //  Intent intent1 = new Intent(v.getContext(),RegistroActivity.class);
+            //startActivityForResult(intent1,0);
+        //}
+    //});
 }
+
--- app/src/main/java/com/jgutierrez/jsparking/RegistroActivity.java
+++ app/src/main/java/com/jgutierrez/jsparking/RegistroActivity.java
@@ -1,10 +1,18 @@
 package com.jgutierrez.jsparking;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistroActivity extends AppCompatActivity {
@@ -14,7 +22,7 @@ public class RegistroActivity extends AppCompatActivity {
     private EditText editTextNombre;
     private EditText editTextTelefono;
     private EditText editTextEmail;
-    private EditText editTextPassword;
+    private EditText editTextClave;
 
     private FirebaseAuth mAuth;
 
@@ -26,7 +34,42 @@ public class RegistroActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        
-    }
+        editTextNombre= (EditText) findViewById(R.id.editTextNombre);
+        editTextTelefono= (EditText) findViewById(R.id.editTextTelefono);
+        editTextEmail= (EditText) findViewById(R.id.editTextEmail);
+        editTextClave= (EditText) findViewById(R.id.editTextClave);
 
+
+        buttonLogin= (Button) findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iniciarResgistro();
+            }
+        });
+
+
+    }
+    private void iniciarResgistro(){
+        final String Nombre =  buttonLogin.getText().toString().trim();
+        String Telefono =  buttonLogin.getText().toString().trim();
+        String Email =  buttonLogin.getText().toString().trim();
+        String Clave =  buttonLogin.getText().toString().trim();
+        if(!TextUtils.isEmpty(Nombre) && !TextUtils.isEmpty(Telefono) &&!TextUtils.isEmpty(Email) &&!TextUtils.isEmpty(Clave)){
+            mAuth.createUserWithEmailAndPassword(Email, Clave)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()){
+                                //startActivity(new Intent(RegistroActivity.this, LoginActivity));
+                                //finish();
+                                Toast.makeText(RegistroActivity.this, "Usuario Registrado ", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+        }
+    }
 }
